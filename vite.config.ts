d643d39b05dc80675da2c2a2ce70969e586a0339
Kidney <kidney@digisynthetic.com>
2026-03-14 23:02:56 +0800

    v1.0.6 add main window

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // 保持这一行，Electron 打包的核心
+  base: './', 
   server: {
     port: 3000,
     host: '0.0.0.0',
